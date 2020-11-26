bcd8f5c268fa4fbeb743cd13ba773f2eed8c51ea
Angelique Shika <angeliqueshika@Shikas-MacBook-Pro.local>
2020-11-26 18:26:35 +0100

    added week 3 codes

---
+++ Week_3/Preclass.py
@@ -0,0 +1,15 @@
+import turtle
+window = turtle.Screen()
+
+color=input("Would you like a lightgreen or red background?")
+window.bgcolor(color)
+
+window.title("Hello!")
+tess = turtle.Turtle()
+tess.color("blue")
+tess.pensize(3)
+tess.forward(50)
+tess.left(120)
+
+tess.forward(50)
+window.mainloop()
---
+++ Week_3/Teenage_mutant.py
@@ -0,0 +1,13 @@
+import turtle
+
+ninja = turtle.Turtle()  # relevant
+screen = turtle.Screen()  # relevant
+
+ninja.forward(200)
+ninja.left(120)
+ninja.forward(200)
+ninja.left(120)
+ninja.forward(200)
+
+screen.exitonclick()  # relevant
+
