a5a7ca79c0e880e3f602b942928319f976f275f4
Angelique Shika <shikaangelique@gmail.com>
2020-12-03 21:41:24 +0100

    minor amendmenments to preclass tasks

--- Week_3/Preclass.py
+++ Week_3/Preclass.py
@@ -14,41 +14,5 @@ tess.left(120)
 tess.forward(50)
 window.mainloop()
 
-number = 100
 
-
-def drawmandy(number):
-    for i in range(number):
-        turtle.penup()
-        turtle.forward(100)
-        turtle.left(90)
-        turtle.penup()
-        turtle.forward(30)
-        turtle.left(90)
-        turtle.pendown()
-        turtle.pencolor("#27ae60")
-        turtle.forward(60)
-        turtle.pencolor("#d35400")
-        turtle.right(70)
-        turtle.forward(70)
-        turtle.right(40)
-        turtle.forward(50)
-        turtle.pencolor("#c0392b")
-        turtle.left(90)
-        turtle.pensize(5)
-        turtle.forward(100)
-        turtle.pencolor("#2980b9")
-        turtle.left(90)
-        turtle.forward(100)
-        turtle.right(50)
-        turtle.forward(70)
-        turtle.pensize(10)
-        turtle.left(60)
-        turtle.pensize(6)
-        turtle.forward(65)
-        turtle.left(45)
-        turtle.forward(120)
-
-
-drawmandy(number)
 
