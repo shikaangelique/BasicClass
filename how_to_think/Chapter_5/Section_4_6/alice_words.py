158afc534abce4a4830cf7f2507b6642a43bc383
Angelique Shika <shikaangelique@gmail.com>
2021-01-01 15:33:26 +0100

    alice stuff

--- how_to_think/Chapter_5/Section_4_6/alice_words.py
+++ how_to_think/Chapter_5/Section_4_6/alice_words.py
@@ -15,6 +15,28 @@ words = alice_text.lower().split()
 word_bank = {}
 for word in words:
     word = word.strip("!\"#$%&'()*+,./:;<=>?@[\]^_`{|}~!”")
+    if len(word) > 0 and word[0].isalpha():  # if the word length is more than 1 and the first letter is an alphabet
+        word_bank[word] = word_bank.get(word, 0) + 1
+
+longest_word = ""
+for word in word_bank.keys():
+    if len(word) > len(longest_word):
+        longest_word = word
+
+table_format = "{:<" + str(len(longest_word)) + "} {:>5}"
+print(table_format.format("Word", "Count"))
+print("=" * (len(longest_word) + 5))
+
+for word, quantity in word_bank.items():
+    print(table_format.format(word, quantity))
+
+with open("alice_words.txt", "w") as alice_output:
+    string_format = "{:<" + str(len(longest_word)) + "} {:>5}\n"
+
+    alice_output.write(string_format.format("Word", "Count"))
+    alice_output.write("=" * (len(longest_word) + 6) + "\n")
+
+    for word in sorted(word_bank.keys()):
+        alice_output.write(string_format.format(word, word_bank[word]))
 
 
-print(words)
